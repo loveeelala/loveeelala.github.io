c03d701470bc23c145a8270b48eefe49183d8eb6
Claudia Verdeja <149544646+loveeelala@users.noreply.github.com>
2025-12-08 23:56:44 -0700

    darkmode

--- wa/final-project/script.js
+++ wa/final-project/script.js
@@ -3,3 +3,20 @@ document.querySelectorAll('.card').forEach(card => {
         card.classList.toggle('expanded');
     });
 });
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+if(localStorage.getItem('theme') === 'dark'){
+    document.body.classList.add('dark-mode');
+}
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        localStorage.setItem('theme', 'dark');
+    }
+    else{
+        localStorage.setItem('theme','light')
+    }
+});
